f27a91b4d1a57b942c94a508a7d297a0a2147374
Edgar <edgar.baudry@gmail.com>
2022-06-12 00:23:32 -0700

    now checking hash 0s

--- public/dashboard/script.js
+++ public/dashboard/script.js
@@ -133,6 +133,11 @@ function verifyAndUpdateBlockChain(bc){
             return {isValid: false, verification:null}; // hash not the same, hell nah
         }
 
+        if(BigInt("0x"+bc[i].hash) < 2n ** (shaBits - zeroBits)){
+            // console.log("prev != prev hash so hell nah")
+            return {isValid: false, verification:null}; // hash not the same, hell nah
+        }
+
         if(!verifyTransactionSignature(bc[i])){
             // console.log("prev != prev hash so hell nah")
             return {isValid: false, verification:null}; // hash not the same, hell nah
